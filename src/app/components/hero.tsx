8c209f197dc3975774d12958de459f4c4349e97f
ash-win-cs <ashwincsuresh@gmail.com>
2024-11-03 01:26:56 +0530

    Update work experience

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,10 +4,28 @@ import { socialHandles } from "./../models/skillsModel";
 import * as Icons from "react-icons/fa";
 
 const socials: Array<socialHandles> = [
-  { id: 1, socialName: "FaGithub", socialUrl: "https://github.com/ash-win-cs" },
-  { id: 2, socialName: "FaEnvelope", socialUrl: "mailto:ashwincsuresh@outlook.com" },
-  { id: 3, socialName: "FaLinkedin", socialUrl: "https://www.linkedin.com/in/ashwin-c-s/" },
-  { id: 4, socialName: "FaWhatsapp", socialUrl: `https://wa.me/+917559055760?text=${encodeURIComponent("Hi, I would like to connect with you!")}` }
+  {
+    id: 1,
+    socialIconName: "FaGithub",
+    socialUrl: "https://github.com/ash-win-cs",
+  },
+  {
+    id: 2,
+    socialIconName: "FaEnvelope",
+    socialUrl: "mailto:ashwincsuresh@outlook.com",
+  },
+  {
+    id: 3,
+    socialIconName: "FaLinkedin",
+    socialUrl: "https://www.linkedin.com/in/ashwin-c-s/",
+  },
+  {
+    id: 4,
+    socialIconName: "FaWhatsapp",
+    socialUrl: `https://wa.me/+917559055760?text=${encodeURIComponent(
+      "Hi, I would like to connect with you!"
+    )}`,
+  },
 ];
 
 const DynamicFaIcon = ({ fname }) => {
@@ -37,13 +55,19 @@ const HeroSection = () => {
             <span className="text-blue-400 text-nowrap">Ashwin C S</span>
           </span>
           <p className="mt-2">
-          I’m a Software Engineer with a passion for creating meaningful solutions. Right now at <strong>GE HealthCare</strong>, I work with Angular, Ionic, and TypeScript to develop mobile applications for clinical usage. I love tackling problems from a broader perspective. To me, software engineering isn’t just about coding; it’s about understanding the entire journey, from development and testing to DevOps.
+            I’m a Software Engineer with a passion for creating meaningful
+            solutions. Right now at <strong>GE HealthCare</strong>, I work with
+            Angular, Ionic, and TypeScript to develop mobile applications for
+            clinical usage. I love tackling problems from a broader perspective.
+            To me, software engineering isn’t just about coding; it’s about
+            understanding the entire journey, from development and testing to
+            DevOps.
           </p>
           <div className="flex flex-row  max-w-xl mx-auto justify-between md:justify-start">
-            {socials.map(({ id, socialName, socialUrl }) => (
+            {socials.map(({ id, socialIconName, socialUrl }) => (
               <div key={id} className="m-5 ml-0 flex items-center ">
                 <a href={socialUrl} target="_blank" rel="noopener noreferrer">
-                  <DynamicFaIcon fname={socialName} />
+                  <DynamicFaIcon fname={socialIconName} />
                 </a>
               </div>
             ))}
