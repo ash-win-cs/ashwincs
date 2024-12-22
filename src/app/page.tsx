1373cc0f56cd44285699b6b304261eb5fbd9c0bc
ash-win-cs <ashwincsuresh@gmail.com>
2024-12-22 09:32:17 +0530

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Footer from "./components/footer";
 import HeroSection from "./components/hero";
 import NavBar from "./components/navbar";
+
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <NavBar/>
-      <HeroSection/>
-      <Footer/>
-    </div>
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <HeroSection />
+        <Footer />
+      </div>
   );
 }
